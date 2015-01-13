935d9d2b2afebed36917a1487805a5f0f778f046
Tom MacWright <tom@macwright.org>
2015-01-12 19:05:43 -0500

    Add JSDoc

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 var ss = require('simple-statistics');
 
+/**
+* Given a FeatureCollection, return the quantiles of a given property
+* @module turf/quantile
+* @param {FeatureCollection} input
+* @param {string} field
+* @param {number} numberOfBreaks
+* @return {Array<number>} the number of breaks
+* @example
+* var points = turf.featurecollection([
+*   turf.point(5,5, {population: 200}),
+*   turf.point(1,3, {population: 600}),
+*   turf.point(14,2, {population: 100}),
+*   turf.point(13,1, {population: 200}),
+*   turf.point(19,7, {population: 300})]);
+* var breaks = turf.quantile(points, 'population', 2);
+* //=breaks
+*/
 module.exports = function(fc, field, percentiles){
   var vals = [];
   var quantiles = [];
 
   fc.features.forEach(function(feature){
     vals.push(feature.properties[field]);
-  })
+  });
   percentiles.forEach(function(percentile){
-    quantiles.push(ss.quantile(vals, percentile * .01));
-  })
-  
+    quantiles.push(ss.quantile(vals, percentile * 0.01));
+  });
   return quantiles;
-}
+};
