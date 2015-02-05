12e36160852c2b08b8b975a9ea264b8b683add5a
Lyzi Diamond <lyzidiamond@gmail.com>
2015-02-04 17:08:47 -0800

    Switch example to geojson primitives

--- index.js
+++ index.js
@@ -8,13 +8,57 @@ var ss = require('simple-statistics');
 * @param {Array<number>} percentiles an Array of percentiles on which to calculate quantile values
 * @return {Array<number>} an array of the break values
 * @example
-* var points = turf.featurecollection([
-*   turf.point([5,5], {population: 5}),
-*   turf.point([1,3], {population: 40}),
-*   turf.point([14,2], {population: 80}),
-*   turf.point([13,1], {population: 90}),
-*   turf.point([19,7], {population: 100})
-* ]);
+* var points = {
+*   "type": "FeatureCollection",
+*   "features": [
+*     {
+*       "type": "Feature",
+*       "properties": {
+*         "population": 5
+*       },
+*       "geometry": {
+*         "type": "Point",
+*         "coordinates": [5, 5]
+*       }
+*     }, {
+*       "type": "Feature",
+*       "properties": {
+*         "population": 40
+*       },
+*       "geometry": {
+*         "type": "Point",
+*         "coordinates": [1, 3]
+*       }
+*     }, {
+*       "type": "Feature",
+*       "properties": {
+*         "population": 80
+*       },
+*       "geometry": {
+*         "type": "Point",
+*         "coordinates": [14, 2]
+*       }
+*     }, {
+*       "type": "Feature",
+*       "properties": {
+*         "population": 90
+*       },
+*       "geometry": {
+*         "type": "Point",
+*         "coordinates": [13, 1]
+*       }
+*     }, {
+*       "type": "Feature",
+*       "properties": {
+*         "population": 100
+*       },
+*       "geometry": {
+*         "type": "Point",
+*         "coordinates": [19, 7]
+*       }
+*     }
+*   ]
+* };
 *
 * var breaks = turf.quantile(
 *   points, 'population', [25, 50, 75, 99]);
